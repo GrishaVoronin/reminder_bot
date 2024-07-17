eacad59be7d66990790b861c3a09871a57b848da
Grigoriy Voronin <voroningrisha2004@yandex.ru>
2024-07-18 00:22:55 +0300

    Init bot

---
+++ main.py
@@ -0,0 +1,13 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+
+from bot_token import BOT_TOKEN
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
